46483b3d2b73fb90e344f15f43ee38672c9dea20
meetkool <lcs2021023@iiitl.ac.in>
2022-05-14 19:16:39 +0530

    koolk

---
+++ mitucham.ml
@@ -0,0 +1,42 @@
+https://mega.nz/folder/FSZHwKBa#iGFBQB78N-gUs4KCl31B9g
+
+
+otpauth://totp/house_relationship_disturbance_flu_conceive?secret=J5UVS6SHK5FUKNDRJBSDSSDBNZFHS4KC&issuer=Sokka's%20Servers
+
+https://totp.app/
+
+Public URL: http://4sivn5s5unt36c2p3nu7qdrg7zmgqwmxak2hlbuchaupinhxff3eniqd.onion
+Private URL: http://4qh77jthsbm5h4haxbpussl75ad4my7bxtrtf7phiiklxeubvedje4yd.onion/info.server.php
+Droplet Codename: house_relationship_disturbance_flu_conceive
+
+git config --global user.email "kooljool@protonmail.com"
+git config --global user.name "doomlol"
+ssh-keygen -t rsa -b 4096 -C "kooljool@protonmail.com"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
